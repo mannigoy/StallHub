55d7165892641a00737697c3c26b6ca19d14a092
Zane Gabrillo <zane.gabrillo30@gmail.com>
2026-05-01 02:31:33 +0800

    Updated Gabrillo-stall feature

--- Stallhub/stalls/urls.py
+++ Stallhub/stalls/urls.py
@@ -4,6 +4,10 @@ from . import views
 app_name = 'stalls'
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('NewStall/', views.add_stall, name='add_stall')
+    path('', views.login_view, name='login'),
+    path('HomePage/', views.add_stall, name='add'),
+    path('index/', views.index, name='index'),
+    path('logout/', views.logout_view, name='logout'),
+    path('edit-profile/', views.edit_profile, name='edit_profile'),
+
 ]
--- Stallhub/stalls/views.py
+++ Stallhub/stalls/views.py
@@ -1,18 +1,69 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 from .forms import StallForm
 from .models import Stall
 
-def index(request):
-    stalls = Stall.objects.all()
-    return render(request, 'stalls/index.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('stalls:add')
+        else:
+            return render(request, 'stalls/login.html', {'error': 'Invalid credentials'})
+
+    return render(request, 'stalls/login.html')
+
+
+def edit_profile(request):
+    user = request.user
+
+    if request.method == "POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        username = request.POST.get("username")
+
+        user.username = username
+        user.email = email
+        user.first_name = name
+
+        user.save()
+
+        return redirect('stalls:edit_profile')
+
+    return render(request, "stalls/edit_profile.html", {"user": user})
+
 
 def add_stall(request):
+    if not request.user.is_authenticated:
+        return redirect('stalls:login')
+
     if request.method == 'POST':
         form = StallForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('stalls:index')
+            return redirect('stalls:add')  # stay on same page
     else:
         form = StallForm()
 
     return render(request, 'stalls/addNewStall.html', {'form': form})
+
+
+def index(request):
+    if not request.user.is_authenticated:
+        return redirect('stalls:login')
+
+    stalls = Stall.objects.all()
+    return render(request, 'stalls/index.html', {'stalls': stalls})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('stalls:login')
+
+
