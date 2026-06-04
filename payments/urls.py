52f58668a9f52482ddf561bfc70ae583f1757a1c
Churchill <cjprequiroso@gmail.com>
2026-06-04 10:40:44 +0800

    added editing

--- payments/urls.py
+++ payments/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('new/', views.add_payment, name='add_payment'),
     path('methods/new/', views.add_payment_method, name='add_payment_method'),
+    path("edit/<int:pk>/", views.edit_payment, name="edit_payment"),
+    path('delete/<int:pk>/', views.delete_payment, name='delete_payment')
 ]
--- payments/views.py
+++ payments/views.py
@@ -1,12 +1,12 @@
 from django.contrib import messages
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, get_object_or_404
 
 from .forms import PaymentForm, PaymentMethodForm
 from .models import Payment, PaymentMethod
 
 
 def index(request):
-    payments = Payment.objects.select_related('agreement', 'payment_method').order_by('-payment_date', '-payment_id')
+    payments = Payment.objects.select_related('agreement', 'payment_method').order_by('payment_date', 'payment_id')
     methods = PaymentMethod.objects.order_by('method_name')
     return render(request, 'payments/index.html', {'payments': payments, 'methods': methods})
 
@@ -35,3 +35,25 @@ def add_payment_method(request):
         form = PaymentMethodForm()
 
     return render(request, 'payments/add_payment_method.html', {'form': form})
+
+def edit_payment(request, pk):
+    payment = get_object_or_404(Payment, pk=pk)
+
+    if request.method == "POST":
+        form = PaymentForm(request.POST, instance=payment)
+        if form.is_valid():
+            form.save()
+            return redirect("payments:index")
+    else:
+        form = PaymentForm(instance=payment)
+
+    return render(request, "payments/edit_payment.html", {"form": form})
+
+def delete_payment(request, pk):
+    payment = get_object_or_404(Payment, pk=pk)
+
+    if request.method == "POST":
+        payment.delete()
+        return redirect("payments:index")
+
+    return redirect("payments:index")  
